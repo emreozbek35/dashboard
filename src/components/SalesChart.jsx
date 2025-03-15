2b6b04989d3b467696308d1153dccc2359b553b6
Emre Özbek <emreozbek002@gmail.com>
2025-03-15 20:18:58 +0300

    align circles

--- src/components/SalesChart.jsx
+++ src/components/SalesChart.jsx
@@ -159,29 +159,29 @@ const SalesChart = () => {
         {/* Right Side - Category Selector */}
         <div className="w-1/4 flex flex-col gap-4 self-center pl-4">
           <button
-            className={`flex items-center gap-2 px-4 py-1 border rounded-full ${
+            className={`max-w-fit px-2 flex items-center gap-2  py-2 border-1 rounded-full ${
               selectedCategory === "All"
-                ? "border-blue-500 text-blue-500 font-bold"
-                : "border-gray-300 text-gray-600"
+                ? "border-[#4a3aff] text-[#4a3aff] font-bold"
+                : "border-gray-400 text-gray-600"
             }`}
             onClick={() => setSelectedCategory("All")}
           >
-            <span className="w-3 h-3 bg-blue-500 rounded-full"></span> All
+            <span className="w-3 h-3 bg-[#4a3aff] rounded-full"></span> All
             Categories
           </button>
           <div className="flex flex-col gap-3">
             {Object.keys(colors).map((category) => (
               <button
                 key={category}
-                className={`flex items-center gap-2 text-sm ${
+                className={`flex px-2 items-center gap-2 text-sm ${
                   selectedCategory === category
-                    ? "text-blue-500 font-bold"
+                    ? "text-[#4a3aff] font-bold"
                     : "text-gray-600"
                 }`}
                 onClick={() => setSelectedCategory(category)}
               >
                 <span
-                  className={`w-3 h-3 rounded-full`}
+                  className="w-3 h-3 rounded-full"
                   style={{ backgroundColor: colors[category] }}
                 ></span>
                 {category} -{" "}
