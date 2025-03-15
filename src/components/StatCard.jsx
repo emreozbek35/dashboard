bc4cf246b2838f7a00a7ad03c7e477e91363ea6f
Emre Özbek <emreozbek002@gmail.com>
2025-03-15 23:29:16 +0300

    chart style change

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -13,24 +13,33 @@ const StatCard = ({
     <div className={`bg-white p-4 rounded-2xl shadow-md ${className}`}>
       <p className="text-[#9291a5] text-sm">Statistics</p>
       <h2 className="text-lg font-bold">{title}</h2>
-      <p className="text-3xl font-bold">{value}</p>
-      <p
-        className={`text-sm font-semibold ${
-          percentage > 0 ? "text-[#04ce00]" : "text-[#ff718b]"
-        }`}
-      >
-        {percentage > 0 ? `+${percentage}%` : `${percentage}%`}
-      </p>
-      <ResponsiveContainer width="100%" height={50}>
-        <LineChart data={data}>
-          <Line
-            type="monotone"
-            dataKey="value"
-            stroke={color}
-            strokeWidth={2}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+
+      <div className="flex flex-row items-center justify-between mt-4">
+        <div>
+          <p className="text-3xl font-bold">{value}</p>
+          <p
+            className={`mt-1 text-sm font-semibold ${
+              percentage > 0 ? "text-[#04ce00]" : "text-[#ff718b]"
+            }`}
+          >
+            {percentage > 0 ? `+${percentage}%` : `${percentage}%`}
+          </p>
+        </div>
+
+        <div className="w-1/2 h-[50px]">
+          <ResponsiveContainer width="100%" height={80}>
+            <LineChart data={data}>
+              <Line
+                type="linear"
+                dataKey="value"
+                stroke={color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 };
